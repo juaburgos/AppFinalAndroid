d8cd831752ba816ea58678324ea994354e4ba598
KarolBurgos <karburgos@umariana.edu.co>
2018-05-27 01:23:29 -0500

    Proyecto final

--- app/src/main/java/isa/appfinalandroid/InicioFragment.java
+++ app/src/main/java/isa/appfinalandroid/InicioFragment.java
@@ -1,19 +1,25 @@
 package isa.appfinalandroid;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import isa.appfinalandroid.models2.EscenariosDeportivos;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class InicioFragment extends Fragment {
-
-
+    private FragmentActivity myContext;
+    private Button btncero;
     public InicioFragment() {
         // Required empty public constructor
     }
@@ -23,7 +29,32 @@ public class InicioFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inicio, container, false);
+        View view= inflater.inflate(R.layout.fragment_inicio, container, false);
+        btncero= (Button) view.findViewById(R.id.button2);
+
+        btncero.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Fragment fragment = null;
+                Class fragmentClass= EscenariosDeportivosFragment.class;
+                try{
+                    fragment = (Fragment) fragmentClass.newInstance();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+                FragmentManager fragmentManager=myContext.getSupportFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+
+            }
+        });
+        return  view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+
+        myContext=(FragmentActivity) context;
+        super.onAttach(context);
     }
 
 }
